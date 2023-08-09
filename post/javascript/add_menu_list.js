fab4c73a62bd7ffa2b2915cd77d8efa15727eeb8
Gr Code Club <135416047+GrCodeClub@users.noreply.github.com>
2023-08-09 18:04:37 +0300

    Update add_menu_list.js

--- post/javascript/add_menu_list.js
+++ post/javascript/add_menu_list.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    var menuLink = ' <li><a href="https://grcodeclub.github.io/system.github.io/">Δημοσιεύσεις</a></li>';
+    var menuLink = ' <li><a href="https://grcodeclub.github.io/site.github.io/post/menu.html">Δημοσιεύσεις</a></li>';
     $('.sidebarMenuInner').append(menuLink);
     menuLink = '<li><a href="https://grcodeclub.github.io/site.github.io/">GrCode Club</a></li>';
     $('.sidebarMenuInner').append(menuLink);
