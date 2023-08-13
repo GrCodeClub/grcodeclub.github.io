9fc1b6bd9821c10a2e929b2ce8df4a1b926e6ad9
Gr Code Club <135416047+GrCodeClub@users.noreply.github.com>
2023-08-13 18:04:25 +0300

    Update menu.js

--- cybersecurity/javascript/menu.js
+++ cybersecurity/javascript/menu.js
@@ -7,6 +7,6 @@ $(document).ready(function() {
     $('.sidebarMenuInner').append(menuLink);
     menuLink = ' <li><a href="https://grcodeclub.github.io/cybersecurity.github.io/post/menu.html">Έγγραφα</a></li>';
     $('.sidebarMenuInner').append(menuLink);
-    menuLink = '<li><a href="https://grcodeclub.github.io/site.github.io/">GrCode Club</a></li>';
+    menuLink = '<li><a href="https://grcodeclub.github.io">GrCode Club</a></li>';
     $('.sidebarMenuInner').append(menuLink);
   });
