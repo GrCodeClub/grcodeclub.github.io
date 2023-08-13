67f8bfeddafa26f168bed428edcf0349a95b1d86
Gr Code Club <135416047+GrCodeClub@users.noreply.github.com>
2023-08-13 17:58:41 +0300

    Create menu.js

---
+++ cybersecurity/javascript/menu.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    var menuLink = ' <li><a href="https://grcodeclub.github.io/cybersecurity.github.io/main.html">Αρχική Σελίδα</a></li>';
+    $('.sidebarMenuInner').append(menuLink);
+    menuLink = ' <li><a href="https://grcodeclub.github.io/cybersecurity.github.io/tools/menu.html">Εργαλεία</a></li>';
+    $('.sidebarMenuInner').append(menuLink);
+    menuLink = ' <li><a href="https://grcodeclub.github.io/cybersecurity.github.io/online_tools/menu.html">Online Tools</a></li>';
+    $('.sidebarMenuInner').append(menuLink);
+    menuLink = ' <li><a href="https://grcodeclub.github.io/cybersecurity.github.io/post/menu.html">Έγγραφα</a></li>';
+    $('.sidebarMenuInner').append(menuLink);
+    menuLink = '<li><a href="https://grcodeclub.github.io/site.github.io/">GrCode Club</a></li>';
+    $('.sidebarMenuInner').append(menuLink);
+  });
