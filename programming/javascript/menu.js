354091551b2d1fa41fe4507510439ea0f39445a6
Gr Code Club <135416047+GrCodeClub@users.noreply.github.com>
2023-08-13 18:10:49 +0300

    Create menu.js

---
+++ programming/javascript/menu.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+    
+    var menuLink = ' <li><a href="#">Προγραμματισμός</a></li>';
+    $('.sidebarMenuInner').append(menuLink);
+
+    menuLink = '<li><a href="#">Έγγραφα </a></li>';
+    $('.sidebarMenuInner').append(menuLink);
+    
+    menuLink = '<li><a href="#">C</a></li>';
+    $('.sidebarMenuInner').append(menuLink);
+    
+    menuLink = '<li><a href="#">C++</a></li>';
+    $('.sidebarMenuInner').append(menuLink);
+    
+    menuLink = '<li><a href="#">Java</a></li>';
+     $('.sidebarMenuInner').append(menuLink);
+    
+    menuLink = '<li><a href="#">Python</a></li>';
+    $('.sidebarMenuInner').append(menuLink);
+
+    
+    menuLink = '<li><a href="#">Website</a></li>';
+    $('.sidebarMenuInner').append(menuLink);
+    
+    
+    menuLink = '<li><a href="https://grcodeclub.github.io">GrCode Club</a></li>';
+    $('.sidebarMenuInner').append(menuLink);
+  });
+  
