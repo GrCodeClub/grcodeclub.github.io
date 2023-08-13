abf1eefed8cc297dcb6e3c4f7e5e0b075cb4a3e1
Gr Code Club <135416047+GrCodeClub@users.noreply.github.com>
2023-08-13 17:15:06 +0300

    Update menu.js

--- system/javascript/menu.js
+++ system/javascript/menu.js
@@ -3,9 +3,9 @@ $(document).ready(function() {
     $('.sidebarMenuInner').append(menuLink);
     menuLink = '<li><a href="https://grcodeclub.github.io/system/windows/menu.html">Windows</a></li>';
     $('.sidebarMenuInner').append(menuLink);
-    menuLink = '<li><a href="https://grcodeclub.github.io/system.github.io/pages/linux/menu.html">Linux</li>';
+    menuLink = '<li><a href="#">Linux</li>';
     $('.sidebarMenuInner').append(menuLink);
-    menuLink = '<li><a href="https://grcodeclub.github.io/system.github.io/pages/post/menu.html">Έγγραφα</a></li>';
+    menuLink = '<li><a href="#">Έγγραφα</a></li>';
     $('.sidebarMenuInner').append(menuLink);
     menuLink = '<li><a href="https://grcodeclub.github.io/site.github.io/">GrCode Club</a></li>';
     $('.sidebarMenuInner').append(menuLink);
