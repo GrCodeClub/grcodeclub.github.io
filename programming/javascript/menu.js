ba34edbc0112674e0959a37ab4265878f9c5d352
Gr Code Club <135416047+GrCodeClub@users.noreply.github.com>
2023-08-15 15:41:18 +0300

    Update menu.js

--- programming/javascript/menu.js
+++ programming/javascript/menu.js
@@ -20,7 +20,6 @@ $(document).ready(function() {
     
     menuLink = '<li><a href="https://grcodeclub.github.io/programming/python.html">Python</a></li>';
     $('.sidebarMenuInner').append(menuLink);
-
     
     menuLink = '<li><a href="https://grcodeclub.github.io/website/main.html">Website</a></li>';
     $('.sidebarMenuInner').append(menuLink);
@@ -34,44 +33,6 @@ $(document).ready(function() {
     menuLink = '<li><a href="https://grcodeclub.github.io">GrCode Club</a></li>';
     $('.sidebarMenuInner').append(menuLink);
 
-
     
-    
-    var menuLink = ' <li><a href="https://grcodeclub.github.io/programming/main.html">Προγραμματισμός</a></li>';
-    $('.sidebarMenuInner').append(menuLink);
-
-    menuLink = '<li><a href="https://grcodeclub.github.io/programming/post.html">Έγγραφα </a></li>';
-    $('.sidebarMenuInner').append(menuLink);
-    
-    menuLink = '<li><a href="https://grcodeclub.github.io/programming/c.html">C</a></li>';
-    $('.sidebarMenuInner').append(menuLink);
-    
-    menuLink = '<li><a href="https://grcodeclub.github.io/programming/c++.html">C++</a></li>';
-    $('.sidebarMenuInner').append(menuLink);
-    
-    menuLink = '<li><a href="https://grcodeclub.github.io/programming/java.html">Java</a></li>';
-     $('.sidebarMenuInner').append(menuLink);
-
-    menuLink = '<li><a href="https://grcodeclub.github.io/post/colab.html">Google Colab</a></li>';
-    $('.sidebarMenuInner').append(menuLink);
-    
-    menuLink = '<li><a href="https://grcodeclub.github.io/programming/python.html">Python</a></li>';
-    $('.sidebarMenuInner').append(menuLink);
-
-    
-    menuLink = '<li><a href="https://grcodeclub.github.io/website/main.html">Website</a></li>';
-    $('.sidebarMenuInner').append(menuLink);
-    
-    menuLink = '<li><a href="#">Docker</a></li>';
-    $('.sidebarMenuInner').append(menuLink);
-    
-    menuLink = '<li><a href="#">Markdown</a></li>';
-    $('.sidebarMenuInner').append(menuLink);
-    
-    menuLink = '<li><a href="https://grcodeclub.github.io">GrCode Club</a></li>';
-    $('.sidebarMenuInner').append(menuLink);
-
-    menuLink = '<li style="height: 250px;"></li>';
-    $('.sidebarMenuInner').append(menuLink);
   });
   
