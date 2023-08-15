87a572e241dd570627c0fab2b2039f5f6a295460
Gr Code Club <135416047+GrCodeClub@users.noreply.github.com>
2023-08-15 15:34:57 +0300

    Update menu.js

--- programming/javascript/menu.js
+++ programming/javascript/menu.js
@@ -34,7 +34,43 @@ $(document).ready(function() {
     menuLink = '<li><a href="https://grcodeclub.github.io">GrCode Club</a></li>';
     $('.sidebarMenuInner').append(menuLink);
 
-    menuLink = '<li style="height: 100px;"></li>';
+    $(document).ready(function() {
+    
+    var menuLink = ' <li><a href="https://grcodeclub.github.io/programming/main.html">Προγραμματισμός</a></li>';
+    $('.sidebarMenuInner').append(menuLink);
+
+    menuLink = '<li><a href="https://grcodeclub.github.io/programming/post.html">Έγγραφα </a></li>';
+    $('.sidebarMenuInner').append(menuLink);
+    
+    menuLink = '<li><a href="https://grcodeclub.github.io/programming/c.html">C</a></li>';
+    $('.sidebarMenuInner').append(menuLink);
+    
+    menuLink = '<li><a href="https://grcodeclub.github.io/programming/c++.html">C++</a></li>';
+    $('.sidebarMenuInner').append(menuLink);
+    
+    menuLink = '<li><a href="https://grcodeclub.github.io/programming/java.html">Java</a></li>';
+     $('.sidebarMenuInner').append(menuLink);
+
+    menuLink = '<li><a href="https://grcodeclub.github.io/post/colab.html">Google Colab</a></li>';
+    $('.sidebarMenuInner').append(menuLink);
+    
+    menuLink = '<li><a href="https://grcodeclub.github.io/programming/python.html">Python</a></li>';
+    $('.sidebarMenuInner').append(menuLink);
+
+    
+    menuLink = '<li><a href="https://grcodeclub.github.io/website/main.html">Website</a></li>';
+    $('.sidebarMenuInner').append(menuLink);
+    
+    menuLink = '<li><a href="#">Docker</a></li>';
+    $('.sidebarMenuInner').append(menuLink);
+    
+    menuLink = '<li><a href="#">Markdown</a></li>';
+    $('.sidebarMenuInner').append(menuLink);
+    
+    menuLink = '<li><a href="https://grcodeclub.github.io">GrCode Club</a></li>';
+    $('.sidebarMenuInner').append(menuLink);
+
+    menuLink = '<li style="height: 250px;"></li>';
     $('.sidebarMenuInner').append(menuLink);
   });
   
