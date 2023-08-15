d00f57503902bafeae62c51137418772d9fa65ed
Gr Code Club <135416047+GrCodeClub@users.noreply.github.com>
2023-08-15 16:17:42 +0300

    Update menu.js

--- network/javascript/menu.js
+++ network/javascript/menu.js
@@ -2,6 +2,9 @@ $(document).ready(function() {
     
     var menuLink = ' <li><a href="https://grcodeclub.github.io/network/main.html">Δίκτυα Υπολογιστών</a></li>';
     $('.sidebarMenuInner').append(menuLink);
+    
+    menuLink = '<li><a href="https://grcodeclub.github.io">GrCode Club</a></li>';
+    $('.sidebarMenuInner').append(menuLink);
 
     menuLink = '<li><a href="https://grcodeclub.github.io/network/κατάλογοι.html">Κατάλογοι</a></li>';
      $('.sidebarMenuInner').append(menuLink);
@@ -12,9 +15,11 @@ $(document).ready(function() {
     
     menuLink = '<li><a href="https://grcodeclub.github.io/network/post.html">Έγγραφα</a></li>';
     $('.sidebarMenuInner').append(menuLink);
+
+    menuLink = '<li><a href="#">Cloud</a></li>';
+    $('.sidebarMenuInner').append(menuLink);
     
-    
-    menuLink = '<li><a href="https://grcodeclub.github.io">GrCode Club</a></li>';
+    menuLink = '<li style="height: 200px;"></li>';
     $('.sidebarMenuInner').append(menuLink);
   });
   
