a8bfc188b6f5bcc59928919c1aeb379b31cada05
Gr Code Club <135416047+GrCodeClub@users.noreply.github.com>
2023-08-15 16:19:14 +0300

    Update menu.js

--- website/javascript/menu.js
+++ website/javascript/menu.js
@@ -1,6 +1,12 @@
 $(document).ready(function() {
     var menuLink = ' <li><a href="https://grcodeclub.github.io/website/main.html">Ιστοσελίδες</a></li>';
     $('.sidebarMenuInner').append(menuLink);
+
+    menuLink = ' <li><a href="https://grcodeclub.github.io">GrCode Club</a></li>';
+    $('.sidebarMenuInner').append(menuLink);
+
+    menuLink = ' <li><a href="https://grcodeclub.github.io/programming/main.html">Γλώσσες Προγραμματισμού</a></li>';
+    $('.sidebarMenuInner').append(menuLink);
     
     menuLink = ' <li><a href="https://grcodeclub.github.io/website/html-css.html">CSS - Html</a></li>';
     $('.sidebarMenuInner').append(menuLink);
@@ -11,11 +17,7 @@ $(document).ready(function() {
     menuLink = ' <li><a href="https://grcodeclub.github.io/website/javascript.html">JavaScript</a></li>';
     $('.sidebarMenuInner').append(menuLink);
 
-    menuLink = ' <li><a href="https://grcodeclub.github.io/programming/main.html">Γλώσσες Προγραμματισμού</a></li>';
+    menuLink = '<li style="height: 200px;"></li>';
     $('.sidebarMenuInner').append(menuLink);
-
-    menuLink = ' <li><a href="https://grcodeclub.github.io">GrCode Club</a></li>';
-    $('.sidebarMenuInner').append(menuLink);
-    
   });
   
