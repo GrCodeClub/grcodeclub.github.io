462698e4d11653cf50d6b8aed3d433d656ab6e14
Gr Code Club <135416047+GrCodeClub@users.noreply.github.com>
2023-08-17 18:57:36 +0300

    Create menu.js

---
+++ tools/menu.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    var menuLink = ' <li><a href="https://grhttps://grcodeclub.github.io/tools/code/image.html">Κωδικας</a></li>';
+    $('.sidebarMenuInner').append(menuLink);
+    menuLink = '<li><a href="https://grcodeclub.github.io/">GrCode Club</a></li>';
+    $('.sidebarMenuInner').append(menuLink);
+
+  });
+  
