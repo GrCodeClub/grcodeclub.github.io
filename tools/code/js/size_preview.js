6fdeb715a2581567da1a8969708ea0a94a98c1cc
Gr Code Club <135416047+GrCodeClub@users.noreply.github.com>
2023-08-18 15:02:59 +0300

    Add files via upload

--- tools/code/js/size_preview.js
+++ tools/code/js/size_preview.js
@@ -1,9 +1,16 @@
 const widthInput = document.getElementById("widthInput");
+const textInput = document.getElementById("codeTextarea");
 
 widthInput.addEventListener("input", function() {
     const width = widthInput.value + "px";
     image.style.width = width;
 });
 
+
+textInput.addEventListener("input", function() {
+    const textWidth = textInput.value.length * 2 + "px"; // You can adjust the multiplier
+    image.style.width = textWidth;
+});
+
 // Ορίστε το αρχικό πλάτος της προεπισκόπησης
-image.style.width = widthInput.value + "px";
+image.style.width = "400" + "px";
