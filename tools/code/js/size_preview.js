5734db712a0e010539f8eb61e151c32c4a52d3ae
Gr Code Club <135416047+GrCodeClub@users.noreply.github.com>
2023-08-18 22:41:25 +0300

    Update size_preview.js

--- tools/code/js/size_preview.js
+++ tools/code/js/size_preview.js
@@ -4,8 +4,10 @@ const textInput = document.getElementById("codeTextarea");
 widthInput.addEventListener("input", function() {
     const width = widthInput.value + "px";
     image.style.width = width;
+    image.style.height = width + "px";
 });
 
 
 // Ορίστε το αρχικό πλάτος της προεπισκόπησης
 image.style.width = "480" + "px";
+image.style.height = "480" + "px";
