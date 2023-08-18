e82c638a4c2ad53971121c3e7560eb19f28464af
Gr Code Club <135416047+GrCodeClub@users.noreply.github.com>
2023-08-18 19:14:23 +0300

    Update preview.js

--- tools/code/js/preview.js
+++ tools/code/js/preview.js
@@ -6,7 +6,7 @@ function updatePreview() {
     document.getElementById("name").value = languageSelect.value;
     const selectedLanguage = languageSelect.value.toLowerCase();; // Χρησιμοποιήστε την επιλεγμένη επιλογή από το dropdown
     const language = Prism.languages[selectedLanguage];
-    preview.innerHTML = '<pre>' + Prism.highlight(codeTextarea.value, language) + '</pre>';
+    preview.innerHTML = '<pre>' + Prism.highlight(codeTextarea.value, language) + '</pre> <br>';
 }
 
 codeTextarea.addEventListener("input", updatePreview);
@@ -18,7 +18,7 @@ updatePreview();
 document.getElementById("generateImage").addEventListener("click", function() {
     html2canvas(image).then(function(canvas) {
         canvas.toBlob(function(blob) {
-            saveAs(blob, "image.png");
+            saveAs(blob, "grcodeclub.png");
         });
     });
 });
