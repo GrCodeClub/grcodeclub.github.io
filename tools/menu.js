c8e9495b3a72cb597e9f97faf428e22a8cb81c25
Gr Code Club <135416047+GrCodeClub@users.noreply.github.com>
2023-08-18 15:17:51 +0300

    Update menu.js

--- tools/menu.js
+++ tools/menu.js
@@ -1,6 +1,11 @@
 $(document).ready(function() {
     var menuLink = ' <li><a href="https://grhttps://grcodeclub.github.io/tools/code/image.html">Κωδικας</a></li>';
     $('.sidebarMenuInner').append(menuLink);
+
+    menuLink = ' <li><a href="https://grhttps://grcodeclub.github.io/tools/convert/ascii.html">Κωδικας</a></li>';
+    $('.sidebarMenuInner').append(menuLink);
+
+    
     menuLink = '<li><a href="https://grcodeclub.github.io/">GrCode Club</a></li>';
     $('.sidebarMenuInner').append(menuLink);
 
