8d6387400840b8653e50f3041550544792e072af
GrCode Club <135416047+GrCodeClub@users.noreply.github.com>
2023-08-21 20:08:00 +0300

    Update footer.js

--- javascript/footer.js
+++ javascript/footer.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
-    var tableHtml = '<table><tr style="border: none;">';
-    tableHtml += '<td style="border: none;"> <a href="https://www.facebook.com/grcode.club/"><img src="https://i.ibb.co/dbjYFzH/fb.png" class="social"/></a> </td>';
-    tableHtml += '<td style="border: none;"> <a href="https://www.instagram.com/grcode_club/"><img src="https://i.ibb.co/j5R6cXx/insta.png" class="social"/></a> </td>';
+    var tableHtml = '<table><tr>';
+    tableHtml += '<td> <a href="https://www.facebook.com/grcode.club/"><img src="https://i.ibb.co/dbjYFzH/fb.png" class="social"/></a> </td>';
+    tableHtml += '<td> <a href="https://www.instagram.com/grcode_club/"><img src="https://i.ibb.co/j5R6cXx/insta.png" class="social"/></a> </td>';
     tableHtml += '</tr></table>';
     $('.add_footer').append(tableHtml);
 });
