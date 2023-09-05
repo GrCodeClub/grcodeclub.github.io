5df8d1af363c2a20405d7801ebbe24815cfc738b
GrCode Club <135416047+GrCodeClub@users.noreply.github.com>
2023-09-06 00:39:23 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,16 +1,11 @@
-
-  // Εμφανίστε ένα παράθυρο επιβεβαίωσης με ένα κουμπί "Εγκατάσταση"
-  Swal.fire({
-    title: 'Εγκαταστήστε την εφαρμογή ως PWA;',
-    icon: 'question',
-    showCancelButton: true,
-    confirmButtonText: 'Εγκατάσταση',
-    cancelButtonText: 'Άκυρο'
-  }).then((result) => {
-    if (result.isConfirmed) {
-      // Εδώ μπορείτε να προσθέσετε τον κώδικα για την εγκατάσταση της PWA
-    } else {
-      alert('Η εφαρμογή δεν εγκαταστάθηκε.');
-    }
-  });
-
+self.addEventListener('fetch', (event) => {
+  // Έλεγχος αν η συσκευή είναι κινητή
+  if (event.request && event.request.headers && event.request.headers.get('User-Agent').includes('Mobile')) {
+    // Εμφάνιση του μηνύματος εγκατάστασης
+    event.respondWith(
+      new Response(JSON.stringify({ message: 'Θέλετε να εγκαταστήσετε την εφαρμογή ως PWA;' }), {
+        headers: { 'Content-Type': 'application/json' },
+      })
+    );
+  }
+});
