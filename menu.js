1dce546f5e201b1e5661193644ef5302a115bafe
GrCode Club <135416047+GrCodeClub@users.noreply.github.com>
2023-09-06 16:17:47 +0300

    Create menu.js

---
+++ menu.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+    
+    var menuLink = ' <li><a href="https://grcodeclub.github.io/network/">Δίκτυα</a></li>';
+    $('.sidebarMenuInner').append(menuLink);
+    
+    menuLink = '<li><a href="https://grcodeclub.github.io/system/">Λειτουργικό Σύστημα</a></li>';
+    $('.sidebarMenuInner').append(menuLink);
+
+    menuLink = '<li><a href="https://grcodeclub.github.io/programming/">Προγραμματισμός</a></li>';
+     $('.sidebarMenuInner').append(menuLink);
+    
+    menuLink = '<li><a href="https://grcodeclub.github.io/cybersecurity/">Κυβερνοασφάλεια</a></li>';
+    $('.sidebarMenuInner').append(menuLink);
+
+    
+    menuLink = '<li><a href="https://grcodeclub.github.io/tools/convert/tool.html">Εργαλεία</a></li>';
+    $('.sidebarMenuInner').append(menuLink);
+
+    menuLink = '<li><a href="https://grcodeclub.github.io/post/menu.html">Δημοσιεύσεις</a></li>';
+    $('.sidebarMenuInner').append(menuLink);
+    
+    menuLink = '<li style="height: 200px;"></li>';
+    $('.sidebarMenuInner').append(menuLink);
+  });
+  
