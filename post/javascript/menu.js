80c4911d2f084d2041ee926e798fd0f477317a50
GrCode Club <135416047+GrCodeClub@users.noreply.github.com>
2023-09-06 17:16:49 +0300

    Update menu.js

--- post/javascript/menu.js
+++ post/javascript/menu.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
-    var menuLink = ' <li><a href="https://grcodeclub.github.io/post/menu.html">Δημοσιεύσεις</a></li>';
+    var  menuLink = '<li><a href="https://grcodeclub.github.io">Αρχική Σελίδα</a></li>';
+    $('.sidebarMenuInner').append(menuLink);
+    
+    menuLink = ' <li><a href="https://grcodeclub.github.io/post/menu.html">Δημοσιεύσεις</a></li>';
     $('.sidebarMenuInner').append(menuLink);
         
     menuLink = ' <li><a href="https://grcodeclub.github.io/post/markdown.html">Markdown</a></li>';
-    $('.sidebarMenuInner').append(menuLink);
-    
-    menuLink = '<li><a href="https://grcodeclub.github.io/">GrCode Club</a></li>';
     $('.sidebarMenuInner').append(menuLink);
 
   });
