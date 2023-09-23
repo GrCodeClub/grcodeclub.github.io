8cede35128b255705a43e31f53dd3f68703fba5d
GrCode Club <135416047+GrCodeClub@users.noreply.github.com>
2023-09-24 00:02:59 +0300

    Update dropdown-content.js

--- js/dropdown-content.js
+++ js/dropdown-content.js
@@ -1,9 +1,15 @@
-// Όταν πατηθεί το κουμπί, εμφανίστε το αναδυόμενο κείμενο
-document.getElementById("showDropdownButton").addEventListener("click", function() {
-  var dropdownContent = document.getElementById("dropdownContent");
-  if (dropdownContent.style.display === "none" || dropdownContent.style.display === "") {
-    dropdownContent.style.display = "block";
-  } else {
-    dropdownContent.style.display = "none";
-  }
-});
+// Προσθέστε ακροατές γεγονότων στα κουμπιά όλων των καρτών
+var showCardButtons = document.querySelectorAll(".showCardButton");
+var cardContents = document.querySelectorAll(".card-content");
+
+for (var i = 0; i < showCardButtons.length; i++) {
+  showCardButtons[i].addEventListener("click", function(event) {
+    var cardContent = this.nextElementSibling;
+
+    if (cardContent.style.maxHeight) {
+      cardContent.style.maxHeight = null;
+    } else {
+      cardContent.style.maxHeight = cardContent.scrollHeight + "px";
+    }
+  });
+}
