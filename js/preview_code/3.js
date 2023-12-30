ebeaf2c0b10ea761fb461ea2d2bfd7ad4fb2aefd
grcodeclub <135416047+GrCodeClub@users.noreply.github.com>
2023-12-30 17:08:05 +0200

    Update 3.js

--- js/preview_code/3.js
+++ js/preview_code/3.js
@@ -1,11 +1,11 @@
 const selectElement3 = document.getElementById("code3");
 selectElement3.addEventListener("change", function() {
-    update_code(selectElement3.value);
+    update_code3(selectElement3.value);
 });
 
 const loadElement3 = document.getElementById("load3");
 
-function update_code(selectedValue) {
+function update_code3(selectedValue) {
     if (selectedValue=="-") loadElement3.innerHTML = '';
     else {
         loadElement3.innerHTML = `<pre data-src=${selectedValue} data-download-link></pre>`;
