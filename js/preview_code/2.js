aa4b8129b5d7d3fd071f0c66b5ad2c9569768b79
grcodeclub <135416047+GrCodeClub@users.noreply.github.com>
2023-12-30 17:04:51 +0200

    Update 2.js

--- js/preview_code/2.js
+++ js/preview_code/2.js
@@ -1,15 +1,14 @@
-const selectElement = document.getElementById("code2");
-selectElement.addEventListener("change", function() {
-    update_code(selectElement.value);
-    selectElement1.value = "-";
+const selectElement2 = document.getElementById("code2");
+selectElement2.addEventListener("change", function() {
+    update_code3(selectElement3.value);
 });
 
-const loadElement = document.getElementById("load2");
+const loadElement2 = document.getElementById("load2");
 
-function update_code(selectedValue) {
-    if (selectedValue=="-") loadElement.innerHTML = '';
+function update_code2(selectedValue) {
+    if (selectedValue=="-") loadElement2.innerHTML = '';
     else {
-        loadElement.innerHTML = `<pre data-src=${selectedValue} data-download-link></pre>`;
+        loadElement2.innerHTML = `<pre data-src=${selectedValue} data-download-link></pre>`;
         Prism.highlightAll();
     }
 }
