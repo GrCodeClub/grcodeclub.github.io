4a8b48529138a1ff600842d2314951d78ef195b5
grcodeclub <135416047+GrCodeClub@users.noreply.github.com>
2023-12-30 16:46:56 +0200

    Update 3.js

--- js/preview_code/3.js
+++ js/preview_code/3.js
@@ -1,12 +1,29 @@
-const selectElement = document.getElementById("code3");
+const selectElement = document.getElementById("code");
 selectElement.addEventListener("change", function() {
     update_code(selectElement.value);
     selectElement1.value = "-";
 });
 
-const loadElement = document.getElementById("load3");
+const loadElement = document.getElementById("load");
 
 function update_code(selectedValue) {
+    if (selectedValue=="-") loadElement.innerHTML = '';
+    else {
+        loadElement.innerHTML = `<pre data-src=${selectedValue} data-download-link></pre>`;
+        Prism.highlightAll();
+    }
+}
+
+
+
+const selectElement = document.getElementById("code3");
+selectElement.addEventListener("change", function() {
+    update_code3(selectElement.value);
+});
+
+const loadElement = document.getElementById("load3");
+
+function update_code3(selectedValue) {
     if (selectedValue=="-") loadElement.innerHTML = '';
     else {
         loadElement.innerHTML = `<pre data-src=${selectedValue} data-download-link></pre>`;
