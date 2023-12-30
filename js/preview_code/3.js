d3456a9526c8399d418271108ca00123c3bfe8e0
grcodeclub <135416047+GrCodeClub@users.noreply.github.com>
2023-12-30 17:00:07 +0200

    Update 3.js

--- js/preview_code/3.js
+++ js/preview_code/3.js
@@ -8,7 +8,7 @@ const loadElement3 = document.getElementById("load3");
 function update_code3(selectedValue) {
     if (selectedValue=="-") loadElement3.innerHTML = '';
     else {
-        loadElement.innerHTML = `<pre data-src=${selectedValue} data-download-link></pre>`;
+        loadElement3.innerHTML = `<pre data-src=${selectedValue} data-download-link></pre>`;
         Prism.highlightAll();
     }
 }
