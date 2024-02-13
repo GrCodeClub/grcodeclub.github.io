88caba8d71be4fd83141d0c89680537872262a30
grcodeclub <135416047+GrCodeClub@users.noreply.github.com>
2024-02-13 23:37:23 +0200

    Update menu.js

--- more/menu.js
+++ more/menu.js
@@ -2,19 +2,19 @@ $(document).ready(function() {
     var  menuLink = '<li><a href="https://grcodeclub.github.io">Αρχική Σελίδα</a></li>';
     $('.sidebarMenuInner').append(menuLink);
     
-    menuLink = ' <li><a href="https://grcodeclub.github.io/post.html">Δημοσιεύσεις</a></li>';
+    menuLink = ' <li><a href="https://grcodeclub.github.io/more">Δημοσιεύσεις</a></li>';
     $('.sidebarMenuInner').append(menuLink);
 
-    menuLink = ' <li><a href="https://grcodeclub.github.io/more/ορολογία.html">Oρολογία</a></li>';
+    menuLink = ' <li><a href="https://grcodeclub.github.io/more/ορολογία">Oρολογία</a></li>';
     $('.sidebarMenuInner').append(menuLink);
 
-    menuLink = ' <li><a href="https://grcodeclub.github.io/more/image/menu.html">Εικόνες</a></li>';
+    menuLink = ' <li><a href="https://grcodeclub.github.io/more/image/index">Εικόνες</a></li>';
     $('.sidebarMenuInner').append(menuLink);
 
-    menuLink = ' <li><a href="https://grcodeclub.github.io/more/google.html">Google</a></li>';
+    menuLink = ' <li><a href="https://grcodeclub.github.io/more/google">Google</a></li>';
     $('.sidebarMenuInner').append(menuLink);
 
-    menuLink = ' <li><a href="https://grcodeclub.github.io/more/office/menu.html">Microsoft Office</a></li>';
+    menuLink = ' <li><a href="https://grcodeclub.github.io/more/office/menu">Microsoft Office</a></li>';
     $('.sidebarMenuInner').append(menuLink);
     
     menuLink = '<li><a href="https://grcodeclub.gr/edit">Edit</a></li>';
