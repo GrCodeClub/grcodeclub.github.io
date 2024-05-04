a7ccdac0e748572619bf94dcafb8459b4e9a8769
grcodeclub <135416047+GrCodeClub@users.noreply.github.com>
2024-05-04 20:05:37 +0300

    Update add_headder_and_menu.js

--- js/add_headder_and_menu.js
+++ js/add_headder_and_menu.js
@@ -1,11 +1,12 @@
 const htmlContent = `
-<section class="show_desktop"> <!--Αρχή Εμφάνιση Υπολογιστή-->
-    <div class="Header"> <!--Αρχή Επικεφαλίδας-->
+<div class="Header"> <!--Αρχή Επικεφαλίδας-->
         <div class="layout" id="header_menu">
             <div><img src="https://grcodeclub.github.io/file/logo_and_graphic/logo.png" class="logo"/></div>
             <div><h1 class="header_text"><a href="https://grcodeclub.github.io/"><span class="header_text">GrCode Club</span></a></h1></div>
         </div>
-    </div> <!--Τέλος Επικεφαλίδας-->
+    </div> <!--Τέλος Επικεφαλίδας--
+    
+<section class="show_desktop"> <!--Αρχή Εμφάνιση Υπολογιστή-->
     <div class="Menu"> <!--Αρχή Menu-->
         <div id="sidebarMenu" style="overflow: auto;">
             <ul class="sidebarMenuInner"></ul>
@@ -14,12 +15,6 @@ const htmlContent = `
 </section> <!--Τέλος Εμφάνιση Υπολογιστή-->
 
 <section class="show_mobile"> <!--Αρχή εμφάνιση σε κινητά-->
-    <div class="Header"> <!--Αρχή Επικεφαλίδας σε κινητά-->
-        <div class="layout">
-            <div><img src="https://grcodeclub.github.io/file/logo_and_graphic/logo.png" class="logo"/></div>
-            <div><h1 class="header_text"><span class="header_text">GrCode Club</span></h1></div>
-        </div>
-    </div> <!--Τέλος Επικεφαλίδας σε κινητά-->
     <input type="checkbox" class="openSidebarMenu" id="openSidebarMenu">
     <label for="openSidebarMenu" class="sidebarIconToggle">
         <div class="spinner diagonal part-1"></div>
