120fae9933327acafbca4606c1ea974c33c8c898
grcodeclub <135416047+GrCodeClub@users.noreply.github.com>
2024-05-07 14:23:05 +0300

    Create head.js

---
+++ js/head.js
@@ -0,0 +1,33 @@
+// Λίστα με τους συνδέσμους προς τα αρχεία CSS
+const cssLinks = [
+    "https://grcodeclub.github.io/css/header_and_footer.css",
+    "https://grcodeclub.github.io/css/main.css",
+    "https://grcodeclub.github.io/css/text.css",
+    "https://grcodeclub.github.io/css/layout.css",
+    "https://grcodeclub.github.io/css/code_bg.css",
+    "https://grcodeclub.github.io/css/prism.css",
+    "https://grcodeclub.github.io/css/list.css",
+    "https://grcodeclub.github.io/css/body.css"
+];
+
+// Αρχικοποίηση head
+const head = document.head;
+
+// Προσθήκη των αρχείων CSS στο head
+cssLinks.forEach(linkUrl => {
+    const linkElement = document.createElement('link');
+    linkElement.rel = 'stylesheet';
+    linkElement.href = linkUrl;
+
+    // Προσθήκη στο head
+    head.appendChild(linkElement);
+});
+
+// Δημιουργία στοιχείου για favicon
+const faviconLink = document.createElement('link');
+faviconLink.rel = 'icon';
+faviconLink.type = 'image/png';
+faviconLink.href = 'https://grcodeclub.github.io/file/logo/black_logo.png';
+
+// Προσθήκη του favicon στο head
+head.appendChild(faviconLink);
